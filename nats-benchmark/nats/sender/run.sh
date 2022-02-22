6ca79ffa1ed9afee3aad581a441237410c442b97
Shivansh Vij <shivanshvij@loopholelabs.io>
2022-02-21 19:23:47 -0500

    Using connection closer

--- nats-benchmark/nats/sender/run.sh
+++ nats-benchmark/nats/sender/run.sh
@@ -8,4 +8,4 @@ go run main.go $1 512 100000 100
 go run main.go $1 4096 50000 100
 go run main.go $1 8192 50000 100
 go run main.go $1 131072 1000 100
-go run main.go $1 1048576 100 10
+go run main.go $1 1048576 100 100
