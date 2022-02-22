d4fd1db7dd396f01adbc509a7eb28ab4df600e13
Shivansh Vij <shivanshvij@loopholelabs.io>
2022-02-21 20:03:01 -0500

    Finalizing

--- nats-benchmark/frisbee/run.sh
+++ nats-benchmark/frisbee/run.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-echo "Running Broker Binding to $1"
-
+echo "Running Broker, binding to $1"
 go run broker/main.go "$1" &
 BROKER_PID=$!
+echo "Broker started with PID $BROKER_PID"
 
 sleep 1
 
-echo "Running Receiver Connecting to $1"
-
+echo "Running Receiver, connecting to $1"
 go run receiver/main.go "$1" &
 RECEIVER_PID=$!
+echo "Receiver started with PID $RECEIVER_PID"
 
 sleep 1
 
-echo "Running Sender Connecting to $1"
+echo "Running Sender. connecting to $1"
 go run sender/main.go "$1" 32 100000 100
 go run sender/main.go "$1" 128 100000 100
 go run sender/main.go "$1" 512 100000 100
@@ -25,3 +25,4 @@ go run sender/main.go "$1" 1048576 100 100
 
 kill -9 $BROKER_PID
 kill -9 $RECEIVER_PID
+pkill main
